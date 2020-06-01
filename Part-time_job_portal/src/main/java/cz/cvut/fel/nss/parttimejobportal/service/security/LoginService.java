aab649851423ddf1c4138f2d94960779fa670851
Marcel Žec <zecmarce@fel.cvut.cz>
2020-06-01 22:00:42 +0200

    login service - translate admin

--- Part-time_job_portal/src/main/java/cz/cvut/fel/nss/parttimejobportal/service/security/LoginService.java
+++ Part-time_job_portal/src/main/java/cz/cvut/fel/nss/parttimejobportal/service/security/LoginService.java
@@ -1,5 +1,6 @@
 package cz.cvut.fel.nss.parttimejobportal.service.security;
 
+import cz.cvut.fel.nss.parttimejobportal.dao.AdminDao;
 import cz.cvut.fel.nss.parttimejobportal.dao.ManagerDao;
 import cz.cvut.fel.nss.parttimejobportal.dto.UserDto;
 import cz.cvut.fel.nss.parttimejobportal.model.Manager;
@@ -22,13 +23,15 @@ public class LoginService {
     private TranslateService translateService;
     private UserDao userDao;
     private ManagerDao managerDao;
+    private AdminDao adminDao;
 
     @Autowired
-    public LoginService(DefaultAuthenticationProvider provider, TranslateService translateService, UserDao userDao, ManagerDao managerDao) {
+    public LoginService(DefaultAuthenticationProvider provider, TranslateService translateService, UserDao userDao, ManagerDao managerDao, AdminDao adminDao) {
         this.provider = provider;
         this.translateService = translateService;
         this.userDao = userDao;
         this.managerDao = managerDao;
+        this.adminDao = adminDao;
     }
 
 
@@ -38,8 +41,10 @@ public class LoginService {
         if (SecurityUtils.getCurrentUserDetails() != null) throw new AlreadyLoginException();
         Authentication auth = new UsernamePasswordAuthenticationToken(/*username*/ email, password);
         provider.authenticate(auth);
-        if (SecurityUtils.getCurrentUser().getRole() == Role.USER ) return translateService.translateUser(userDao.find(SecurityUtils.getCurrentUser().getId()));
-        else return translateService.translateManager(managerDao.find(SecurityUtils.getCurrentUser().getId()));
+        Role role = SecurityUtils.getCurrentUser().getRole();
+        if( role == Role.MANAGER) return translateService.translateManager(managerDao.find(SecurityUtils.getCurrentUser().getId()));
+        else if( role == Role.ADMIN) return translateService.translateAdmin(adminDao.find(SecurityUtils.getCurrentUser().getId()));
+        return translateService.translateUser(userDao.find(SecurityUtils.getCurrentUser().getId()));
 
     }
 
