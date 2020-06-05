d404926d6d4e981d8ae577334a1039cb3b32533b
Marcel Žec <zecmarce@fel.cvut.cz>
2020-06-05 11:51:44 +0200

    comments in RequestTimeInterceptor

--- Part-time_job_portal/src/main/java/cz/cvut/fel/nss/parttimejobportal/rest/interceptor/RequestTimeInterceptor.java
+++ Part-time_job_portal/src/main/java/cz/cvut/fel/nss/parttimejobportal/rest/interceptor/RequestTimeInterceptor.java
@@ -10,6 +10,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * RequestTimeInterceptor - class measure time of requests and logs it.
+ */
 @Component
 public class RequestTimeInterceptor implements HandlerInterceptor {
 
@@ -17,17 +20,41 @@ public class RequestTimeInterceptor implements HandlerInterceptor {
     private long startTime;
     private long stopTime;
 
+    /**
+     * preHandle method start measure nanoTime at the beginning of request.
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         startTime = System.nanoTime();
         return true;
     }
 
+    /**
+     * postHandle method end measure nanoTime at the end of request.
+     * @param request
+     * @param response
+     * @param handler
+     * @param modelAndView
+     * @throws Exception
+     */
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         stopTime = System.nanoTime();
     }
 
+    /**
+     * afterCompletion count difference between start and end time and log this time.
+     * @param request
+     * @param response
+     * @param handler
+     * @param exception
+     * @throws Exception
+     */
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception exception) throws Exception {
         double time = (double) (stopTime - startTime) / 1_000_000_000;
